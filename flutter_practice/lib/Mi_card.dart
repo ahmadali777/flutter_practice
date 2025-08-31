7206372d129daeef821b9ac789192f7de60ed5aa
Ahmad <aalibest007@gmail.com>
2025-08-31 11:46:22 +0500

    new files added

---
+++ flutter_practice/lib/Mi_card.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+ const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mi card', textAlign: TextAlign.center, style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),),
+      ),
+      body: Column(
+        children: [
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage('images/my-pic.png'),
+          ),
+          Text('Muhammad Ahmad Ali' ,
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+          ),
+          Text('Flutter Developer' ,
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.grey,
+          ),
+          ),
+          Divider(
+            height: 30,
+            thickness: 2,
+            indent: 50,
+            endIndent: 50,
+            color: Colors.grey,
+          ),
+          Card(
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: ListTile(
+              leading: Icon(Icons.phone, color: Colors.lightBlue),
+              title: Text('+123 456 7890'),
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: ListTile(
+              leading: Icon(Icons.email, color: Colors.lightBlue),
+              title: Text('aalibest007@gmail.com'),),
+          ),
+        ],
+      )
+    );
+  }
+}
